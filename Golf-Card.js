0756f0937309ad8a356d0f97b4fe764aa06b26fb
arianneglick <arianne.glick@yahoo.com>
2016-11-13 20:43:08 -0700

    fixed add player

--- Golf-Card.js
+++ Golf-Card.js
@@ -28,66 +28,77 @@ function loadCourses(theid){
         selectedCourse = JSON.parse(data);
         $("#courseName").html(selectedCourse.course.name);
         for(var i = 0; i < (selectedCourse.course.holes[0].tee_boxes.length - 1); i++){
-            $("#teeType").append("<option value='" + i + "'>"+ selectedCourse.course.holes[0].tee_boxes[i].tee_type +"</option>"); //something here isn't working quite right. "value" is appearing next to the tee
+            $("#teeType").append("<option value='" + i + "'>"+ selectedCourse.course.holes[0].tee_boxes[i].tee_type +"</option>");
         }
     });
 }
 
+for(var pl = 1; pl <= numplayers; pl++ ) {
+    playercollection += "<div id='player" + pl + "' class='holebox playerbox'> Player " + pl + " <span onclick='deleteplayer(" + pl + ")' class='deletebtn glyphicon glyphicon-minus-sign'></span></div>";
+    grandtotalcollection += "<div id='grand" + pl + "' class='holebox'>0</div>";
+}
+function calculatescore(theplayer){
+    var thetotal = 0;
+    for(var t = 1; t <= numholes; t++){
+        thetotal += Number($("#player" + theplayer + "hole" + t).val());
+    }
+    $("#grand" + theplayer).html(thetotal);
+}
+
+function deleteplayer(playerid){
+    $("#player" + playerid).remove();
+    $("#grand" + playerid).remove();
+    for(var p = 1; p <= numholes; p++){
+        $("#player" + playerid + "hole" + p).remove();
+    }
+}
+
+function myCreateFunction() {
+    var table = document.getElementById("scoreTable");
+    var row = table.insertRow(scoreTable.length);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var cell7 = row.insertCell(6);
+    var cell8 = row.insertCell(7);
+    var cell9 = row.insertCell(8);
+    var cell10 = row.insertCell(9);
+    var cell11 = row.insertCell(10);
+    var cell12 = row.insertCell(11);
+    var cell13 = row.insertCell(12);
+    var cell14 = row.insertCell(13);
+    var cell15 = row.insertCell(14);
+    var cell16 = row.insertCell(15);
+    var cell17 = row.insertCell(16);
+    var cell18 = row.insertCell(17);
+    var cell19 = row.insertCell(18);
+    var cell20 = row.insertCell(19);
+
+}
+
+function myDeleteFunction() {
+    document.getElementById("myTable").deleteRow(0);
+}
+
+
 function beginCard(){
-    var players = $("#playerCount").val();
+
     $("#leftCard").html("");
     $("#wholeCard").css("display","flex");
-    for(var i = 0; i < players; i++){
-        var playerLabel = "<div id='playerLabel" + i + "'>" +
-                "Player "+ (i + 1) +" " +
-                "<span class='glyphicon glypicon-minus-sign' onclick='removePlayer("+ i +")'></span>"
-            "</div>";
-        $("#leftCard").append(playerLabel);
-    }
     for(var c = 1; c <= numberOfHoles; c++){
         $("#rightCard").append("<div id='column" + c +"' class='holeColumn'><div class='columnHeader'>" + c +"</div></div>");
     }
-    $(".modalBackground").fadeOut();
+
 }
-
-
-function removePlayer(theId){
-    $("#playerLabel" + theId).remove();
-}
-
-//Get the geolocation
-/*
-function getGeolocation(){
-    return new Promise(function (resolve, reject) {
-        var options = {
-            enableHighAccuracy: true
-        };
-        function success(pos){
-            var coordinates = pos.coords;
-
-            console.log('Your current position is: ');
-            console.log('Latitude: ' + coordinates.latitude);
-            console.log('Longitude' + coordinates.longitude);
-            console.log('More or less ' + coordinates.accuracy + ' meters.');
-
-            resolve(coordinates);
-        }
-        function error(error){
-            console.warn('ERROR(' + error.code + '): ' + error.message);
-            reject(error);
-        }
-        navigator.geolocation.getCurrentPosition(success, error, options);
-    })
-}
-
-function showLoadingScreen(){
-    $('#loading-screen').show();
-}
-
-function hideLoadingScreen(){
-    $('#loading-screen').hide();
-}
-
-function createCourseSelect(){
-
+/*function calculatescore(theplayer){
+    var thetotal = 0;
+    for(var t = 1; t <= numholes; t++){
+        thetotal += Number($("#player" + theplayer + "hole" + t).val());
+    }
+    $("#grand" + theplayer).html(thetotal);
 }*/
+
+
